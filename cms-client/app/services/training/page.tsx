8e85e225ef1214aa5b6482a3e36aa7bdb3f94543
mattv993 <mattv@q4inc.com>
2023-06-10 10:30:06 -0400

    training page setup

---
+++ cms-client/app/services/training/page.tsx
@@ -0,0 +1,7 @@
+export default function Training() {
+  return (
+    <section className="flex flex-col items-center justify-between p-24">
+      <h1>Training content here..</h1>
+    </section>
+  )
+}
