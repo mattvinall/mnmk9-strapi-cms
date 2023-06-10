abe5e28fc83c9dd3bf5bdc84ff86280a33d638ec
mattv993 <mattv@q4inc.com>
2023-06-10 09:56:47 -0400

    handle close logic, added logo

--- cms-client/app/components/Navigation.tsx
+++ cms-client/app/components/Navigation.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from 'react';
 import Link from 'next/link';
+import Image from "next/image";
+import Logo from "../../public/mnmk9-logo.png";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,20 +12,24 @@ const Navigation = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  }
+
   return (
     <nav className="bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <Link className="text-white text-xl font-semibold" href="/">
-              Logo
+              <Image className="rounded-full" src={Logo} width={75} height={75} alt="MNM-K9 company logo" />
             </Link>
           </div>
           <div className="md:hidden">
             <button
               type="button"
               className="text-gray-300 hover:text-white focus:outline-none focus:text-white"
-              onClick={toggleMenu}
+              onClick={isOpen ? closeMenu : toggleMenu}
             >
               <svg
                 className="h-6 w-6 fill-current"
@@ -37,30 +43,30 @@ const Navigation = () => {
                     d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm0 5h16v2H4v-2z"
                   />
                 ) : (
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm0 5h16v2H4v-2z"
-                    />
-                  )}
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm0 5h16v2H4v-2z"
+                  />
+                )}
               </svg>
             </button>
           </div>
-          <div className={`hidden md:block ${isOpen ? 'block' : 'hidden'}`}>
-            <div className="ml-10 flex items-baseline space-x-8">
-              <Link href="/">
+          <div className={`fixed top-10 overflow-y-none right-0 w-1/3 md:w-2/3 lg:w-1/2 h-screen bg-gray-900 transform transition-transform duration-300 ease-in-out ${isOpen ? 'translate-x-0' : 'translate-x-full'} md:static md:translate-x-0 md:flex md:flex-row md:space-x-8 md:bg-transparent md:h-auto`}>
+            <div className="mt-16 space-y-8 text-center md:mt-0 md:space-y-0 md:flex md:flex-row md:space-x-8">
+              <Link className="block py-2 px-4 text-white hover:bg-gray-800 md:inline-block" href="/" onClick={closeMenu}>
                 Home
               </Link>
-              <Link href="/about">
+              <Link className="block py-2 px-4 text-white hover:bg-gray-800 md:inline-block" href="/about" onClick={closeMenu}>
                 About
               </Link>
-              <Link href="/services">
+              <Link className="block py-2 px-4 text-white hover:bg-gray-800 md:inline-block" href="/services" onClick={closeMenu}>
                 Services
               </Link>
-              <Link href="/gallery">
+              <Link className="block py-2 px-4 text-white hover:bg-gray-800 md:inline-block" href="/gallery" onClick={closeMenu}>
                 Gallery
               </Link>
-              <Link href="/contact">
+              <Link className="block py-2 px-4 text-white hover:bg-gray-800 md:inline-block" href="/contact" onClick={closeMenu}>
                 Contact
               </Link>
             </div>
