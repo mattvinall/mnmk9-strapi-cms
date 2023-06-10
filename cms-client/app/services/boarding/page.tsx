7fec33cdcef805fb9cc96bbc1906f4870b118748
mattv993 <mattv@q4inc.com>
2023-06-10 10:29:00 -0400

    boarding page setup

---
+++ cms-client/app/services/boarding/page.tsx
@@ -0,0 +1,7 @@
+export default function Boarding() {
+  return (
+    <section className="flex flex-col items-center justify-between p-24">
+      <h1>Boarding content here..</h1>
+    </section>
+  )
+}
