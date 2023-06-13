8e0ff160e100627b24a0427b2391afc81cfe36c6
mattv993 <mattv@q4inc.com>
2023-06-13 14:45:39 -0400

    remove console.log

--- cms-client/app/blog/[...slug]/page.tsx
+++ cms-client/app/blog/[...slug]/page.tsx
@@ -44,8 +44,6 @@ const getBlogPost = async (slug: string) => {
   const url = "http://127.0.0.1:1337"
   const res = await fetch(`${url}/api/posts?filters[slug][$eq]=${slug}`);
 
-  console.log("response from api slug", res)
-
   if (!res?.ok) {
     throw new Error("Failed to fetch data from API. Error getting blog posts");
   }
