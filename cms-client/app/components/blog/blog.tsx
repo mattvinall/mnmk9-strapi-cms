8f748146af1a3292d2674658eae3ac7333fdfcce
mattv993 <mattv@q4inc.com>
2023-06-13 15:52:25 -0400

    removed image markup, not coming through from strapi

--- cms-client/app/components/blog/blog.tsx
+++ cms-client/app/components/blog/blog.tsx
@@ -1,29 +1,21 @@
 import { Post } from '@/app/types/post';
+import { truncateBody } from '@/app/utils/truncateBody';
 import Link from 'next/link';
 
 const BlogPosts = ({ attributes }: Post) => {
 
   if (!attributes) return <p>Loading...</p>
 
-  const { title, author, createdAt, image, slug, body } = attributes;
-  console.log("image", image)
+  const { title, author, createdAt, slug, body } = attributes;
+
   return (
     <Link href={`/blog/${slug}`}>
-      <div className="flex flex-col rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300">
-        {image && (
-          <figure className="relative pb-2/3">
-            <img
-              className="absolute object-cover w-full h-full"
-              src={image}
-              alt={title}
-            />
-          </figure>
-        )}
+      <div className="rounded-lg min-h-[175px] shadow-lg hover:shadow-2xl transition-shadow duration-300">
         <div className="px-6 py-4 flex-grow">
           <h2 className="text-2xl font-semibold mb-2">{title}</h2>
           <p className="text-gray-600 text-sm mb-2">By {author}</p>
           <p className="text-gray-500 text-sm mb-2">{new Date(createdAt as Date).toLocaleDateString("en")}</p>
-          <p className="text-gray-700">{body}</p>
+          <p className="text-gray-700">{truncateBody(body as string, 25)}</p>
         </div>
       </div>
     </Link>
