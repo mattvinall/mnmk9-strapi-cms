8477f08e330a2ca1b0a7f1cbe7267b9ef63dcaf7
mattv993 <mattv@q4inc.com>
2023-06-13 14:45:30 -0400

    created BlogPosts component and mapped over data and returned the component and passed down each post as props

--- cms-client/app/blog/page.tsx
+++ cms-client/app/blog/page.tsx
@@ -1,18 +1,34 @@
+import { Post } from "../types/post";
+import BlogPosts from "../components/blog/blog";
+
 export default async function Blog() {
 
-  const blogPosts = await getBlogPosts();
+  const posts = await getBlogPosts();
+
+  const blogPosts = posts.data;
+
+  console.log("blog posts", blogPosts);
+
+  if (!blogPosts || blogPosts.length === 0) return;
 
   return (
-    <section className="flex flex-col items-center justify-between p-24">
+    <section className="flex flex-col items-center justify-between p-12">
       <h1>Blog</h1>
       {/* blog posts go here */}
+      {blogPosts?.map((post: Post) => {
+        const { attributes } = post
+        const { slug } = attributes;
+        return (
+          <BlogPosts key={slug} attributes={attributes} />
+        )
+      })}
     </section>
   )
 }
 
 const getBlogPosts = async () => {
   const url = "http://127.0.0.1:1337";
-  const res = await fetch(`${url}/api/posts`);
+  const res = await fetch(`${url}/api/posts`, { cache: 'no-store' });
 
   if (!res?.ok) {
     throw new Error("Failed to fetch data from API. Error getting blog posts");
