611be5cbbe5ecb1d4fc6e7ddbd099c8d0c65c2dd
mattv993 <mattv@q4inc.com>
2023-06-13 16:30:31 -0400

    style update

--- cms-client/app/components/blog/blog.tsx
+++ cms-client/app/components/blog/blog.tsx
@@ -10,10 +10,10 @@ const BlogPosts = ({ attributes }: Post) => {
 
   return (
     <Link href={`/blog/${slug}`}>
-      <div className="rounded-lg min-h-[175px] shadow-lg hover:shadow-2xl transition-shadow duration-300">
+      <div className="rounded-lg min-h-[175px] shadow-lg hover:drop-shadow-xl  transition-drop-shadow duration-300">
         <div className="px-6 py-4 flex-grow">
           <h2 className="text-2xl font-semibold mb-2">{title}</h2>
-          <p className="text-gray-600 text-sm mb-2">By {author}</p>
+          <p className="text-gray-600 text-sm mb-2">By: {author}</p>
           <p className="text-gray-500 text-sm mb-2">{new Date(createdAt as Date).toLocaleDateString("en")}</p>
           <p className="text-gray-700">{truncateBody(body as string, 25)}</p>
         </div>
