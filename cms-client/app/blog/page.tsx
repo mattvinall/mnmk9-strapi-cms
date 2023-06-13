c46ef3976d35cb1513589867dbb83dfab45d5f2e
mattv993 <mattv@q4inc.com>
2023-06-13 10:43:38 -0400

    blog post home page

--- cms-client/app/blog/page.tsx
+++ cms-client/app/blog/page.tsx
@@ -1,7 +1,22 @@
-export default function Blog() {
+export default async function Blog() {
+
+  const blogPosts = await getBlogPosts();
+
   return (
     <section className="flex flex-col items-center justify-between p-24">
       <h1>Blog</h1>
+      {/* blog posts go here */}
     </section>
   )
+}
+
+const getBlogPosts = async () => {
+  const url = "http://127.0.0.1:1337";
+  const res = await fetch(`${url}/api/posts`);
+
+  if (!res?.ok) {
+    throw new Error("Failed to fetch data from API. Error getting blog posts");
+  }
+
+  return res.json();
 }
